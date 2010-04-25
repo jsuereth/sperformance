a28a2a4dfb2ce4212f07910a992172ad02932f79
Joshua.Suereth@gmail.com <jsuereth@jsuereth-laptop.(none)>
2010-04-25 15:49:15 -0400

    Updated build config for scala 2.8.0 trunk

--- project/build/SPerformance.scala
+++ project/build/SPerformance.scala
@@ -2,6 +2,7 @@ import sbt._
 
 class SPerformanceProject(info: ProjectInfo) extends DefaultProject(info)
 {
+  val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
   lazy val hi = task { println("Hello World"); None }
 
   val jfreechart = "jfree" % "jfreechart" % "1.0.12"
