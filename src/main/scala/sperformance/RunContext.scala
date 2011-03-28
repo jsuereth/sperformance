cf59994402ed0647cac5264d622aeb9ab3235ff7
jeichar <jesse.eichar@camptocamp.com>
2011-03-28 13:58:48 +0200

    adding CSV output option

--- src/main/scala/sperformance/RunContext.scala
+++ src/main/scala/sperformance/RunContext.scala
@@ -1,10 +1,14 @@
 package sperformance
 
 import charting.Charting
+import intelligence.Cluster
 import org.jfree.chart.{ChartUtilities, JFreeChart}
 import collection.mutable.ListBuffer
 import java.io.{FileOutputStream, BufferedOutputStream, PrintStream, File}
 import util.FileUtils
+import java.net.URL
+import scalax.io.Resource
+import scalax.io.Codec
 
 /**
  * Abstract interface designed to allow customize where reports go and how they are generated. (i.e. could be sent to a Swing UI).
@@ -107,5 +111,39 @@ class DefaultRunContext(val outputDirectory : File, testName : String) extends R
     }  finally {
       output.close()
     }
+  }
+}
+
+/**
+ * Generates no charts, instead this writes the results to a file so that tests can be tracked
+ * and charted over time
+ */
+class CSVRunContext(outputFile:File) extends RunContext {
+
+  val testContext = new intelligence.ClusterResults
+  def writeResultingChart(clusterName : List[String], chartName : String, chart : JFreeChart) : Unit = {
+    // this doesn't write charts
+  }
+
+  private def makeSeriesName(cluster:Cluster)(result : PerformanceTestResult) = cluster.makeName(result.attributes)
+
+  def writeResults() = {
+    outputFile.getParentFile.mkdirs
+    Resource.fromFile(outputFile).writer(Codec.UTF8).acquireFor {
+      writer =>
+        for {
+          (md, cluster) <- testContext.clusters
+          val clusterName = md.attributes.mkString("-")
+          (name,results) <- cluster.results.groupBy(makeSeriesName(cluster) _)
+          result <- results
+            (axisName,axisData) <- result.axisData
+        } {
+          val rowData = (clusterName, name,axisName,axisData,result.time)
+          val rowAsStrings = rowData.productIterator map {_.toString} map {_.replace("\"","\\\"")}
+          writer.write(rowAsStrings mkString ("\"","\",\"","\""))
+          writer.write("\n")
+        }
+    }
+
   }
 }
